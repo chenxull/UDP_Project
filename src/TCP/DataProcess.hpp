9d2117254d225e55828c0945da6105ee05fc70f7
chenxull <947584280@qq.com>
2018-10-22 00:05:05 +0800

    生成随机数据的头文件

---
+++ src/TCP/DataProcess.hpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+class DataProduct{
+    private:
+    void rand_msg_buff(char **str,short * strlen);
+
+};
+
+void rand_msg_buff(char **str, short * strlen) {
+    int i;
+    int len;
+    srand((int)time(0)); // 设置种子，不然每次rand执行结果一样
+    len = rand() % 999;
+    *strlen = len;
+    char *p = *str;
+    srand((int)time(0));
+    for (i = 0; i < len; ++i){
+        p[i] = 'A' + rand() % 26;
+    } 
+    p[i] = '\0';
+}
